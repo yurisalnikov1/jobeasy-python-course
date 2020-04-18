80cd511b089554632a647600c4997c7410f35753
yurisalnikov1 <yurisalnikov1@gmail.com>
2020-04-17 23:35:32 -0400

    updating hw 2_1

---
+++ lesson_2/__init__.py
@@ -0,0 +1,19 @@
+# Python Title() Method Example 
+
+str1 = 'geeKs foR geEks'
+str2 = str1.title() 
+print (str2)
+
+# observe the original string 
+print  (str1.title())
+print  (str1)
+
+# Performing title() function directly 
+str3 = 'ASIPU pawan kuMAr'.title()
+print (str3)
+
+str4 = 'stutya kUMari sHAW'.title() 
+print (str4)
+
+
+
