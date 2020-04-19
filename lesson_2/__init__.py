937146b6edd9d4f3cfa4e4c6700598159a7e827a
yurisalnikov1 <yurisalnikov1@gmail.com>
2020-04-18 23:59:38 -0400

    updating hw 2 all sections

--- lesson_2/__init__.py
+++ lesson_2/__init__.py
@@ -1,19 +1,14 @@
 # Python Title() Method Example 
 
-str1 = 'geeKs foR geEks'
-str2 = str1.title() 
-print (str2)
+# str1 = 'geeKs foR geEks'
+# str2 = str1.title()
+# print (str2)
 
 # observe the original string 
-print  (str1.title())
-print  (str1)
 
-# Performing title() function directly 
-str3 = 'ASIPU pawan kuMAr'.title()
-print (str3)
+line_1="apples"
+line_2="red"
+line_3="blue"
 
-str4 = 'stutya kUMari sHAW'.title() 
-print (str4)
-
-
-
+result_1=f'{line_1.title()} can be {line_2} and {line_3}'
+print(result_1)
