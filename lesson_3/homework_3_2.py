f703644de57e3f0a2bddd80e295c1e1115b7f380
yurisalnikov1 <yurisalnikov1@gmail.com>
2020-04-26 21:58:23 -0400

    updating hw 3.2

--- lesson_3/homework_3_2.py
+++ lesson_3/homework_3_2.py
@@ -5,8 +5,16 @@
 # If number is  greater than 15 and less than or equal to 20, save the number raised to the power 3 to result_1
 # Else save the text "Wrong value" to result_1
 
-number = None
-result_1 = None
+if 0> number <= 7:
+    result_1 == f'(number) * 10'
+if 7> number <= 15:
+    result_1 == f'(number) \\ 3'
+if 15> number <=20
+    result_1 == f'(number) ** 3'
+else:
+    result_1 == f'Wrong value'
+
+
 
 
 # Input two numbers between 1 and 10, save this values to number_1 variable and number_2 variables.
@@ -17,25 +25,35 @@ result_1 = None
 # If both numbers are greater than 5 and less than or equal to 10, multiply their sum by 3 and save it to result_2
 # Else save the text "Wrong values, try again" to result_2
 
-number_1 = None
-number_2 = None
-result_2 = None
-
+if 0 < number_1 >= 5 and 0 < number_2 >= 5:
+    result_2 == f'number_1 * number_2'
+if (0 < number_1 <= 5 and 5 < number_2 <= 10) or (0 < number_2 <= 5 and 5 < number_1 <= 10):
+    result_2 == f'number_1 + number_2'
+if 5 < number_1 <= 10 and 5 < number_2 <= 10:
+    result_2 == f'(number_1 + number_2) *3'
+else:
+    result_2 == f'Wrong values, try again'
 
 # Input your first name and save it to first_name variable,
 # then input last name and save it to last_name
 # If first_name or last_name are shorter than 6 characters, save a full name (with a space between) to result_3
 # Else save first_name to result_3 as many times as length of last_name value
 
-first_name = None
-last_name = None
-result_3 = None
-
+if len(first_name) <6 or len(last_name) <6:
+    result_3 == f'{first_name} {last_name}'
+else:
+    result_3 == f'{first_name}' * len(last_name)
 
 # Input a random number. Save this value to random_number variable
 # If this number is less 10 or greater than 99, save the text "Please, put in a number between 10 and 99" to result_4
 # If a number doesn't meet the first condition, find the remainder of random_value divided by 2.
 # If it is 0, save the text "Even number" to result_4 , else save the message "Odd number"
 
-random_number = None
-result_4 = None
+if random_number  < 10  or random_number > 99:
+    result_4 == "Please, put in a number between 10 and 99"
+else:
+        if random_number % 2 == 0:
+            result_4 == "Even number"
+        else:
+            result_4 == "Odd number"
+
